714b3139884213e2957749e43ee4f3015349dad2
Pokemon918 <j27394645@gmail.com>
2024-08-20 17:49:08 -0700

    feat: pdf loader

--- index.py
+++ index.py
@@ -1,44 +1,17 @@
+import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
 
-from langchain_openai import ChatOpenAI
-from langchain.schema import SystemMessage
-from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
-from langchain.memory import ConversationBufferMemory
-from langchain_community.chat_message_histories import FileChatMessageHistory
+def load_document(file):
+    from langchain_community.document_loaders import PyPDFLoader
+    print(f'Loading {file}')
+    loader = PyPDFLoader(file)
+    data = loader.load()
+    return data
 
-history = FileChatMessageHistory('chat_history.json')
-memory = ConversationBufferMemory(
-    memory_key='chat_history',
-    chat_memory=history,
-    return_messages=True
-)
+data = load_document('book.pdf')
+print(data[1].page_content)
+print(data[10].metadata)
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
-
-prompt = ChatPromptTemplate(
-    input_variables=['content'],
-    messages=[
-        SystemMessage(content='You are chatbot having a conversation with a human.'),
-        MessagesPlaceholder(variable_name='chat_history'),
-        HumanMessagePromptTemplate.from_template('{content}')
-    ]
-)
-
-chain = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    memory=memory,
-    verbose=False
-)
-
-while True:
-    content = input('Your prompt: ')
-    if content in ['quit', 'exit', 'bye']:
-        print('Goodbye!')
-        break
-
-    response = chain.run({'content': content})
-    print(response)
-    print('-' * 50)
+print(f'You have {len(data)} pages in your data')
+print(f'There are {len(data[20].page_content)} characters in the page')
