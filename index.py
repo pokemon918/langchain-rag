0a649e266a020132554160aa63c60fca4531f254
Pokemon918 <j27394645@gmail.com>
2024-08-20 18:05:07 -0700

    add: wikipedia loader

--- index.py
+++ index.py
@@ -20,9 +20,18 @@ def load_document(file):
     data = loader.load()
     return data
 
-data = load_document('book.pdf')
-print(data[1].page_content)
-print(data[10].metadata)
+def load_from_wikipedia(query, lang='en', load_max_docs=2):
+    from langchain_community.document_loaders import WikipediaLoader
+    loader = WikipediaLoader(query=query, lang=lang, load_max_docs=load_max_docs)
+    data = loader.load()
+    return data
 
-print(f'You have {len(data)} pages in your data')
-print(f'There are {len(data[20].page_content)} characters in the page') 
+# data = load_document('book.pdf')
+# print(data[1].page_content)
+# print(data[10].metadata)
+
+# print(f'You have {len(data)} pages in your data')
+# print(f'There are {len(data[20].page_content)} characters in the page') 
+
+data = load_from_wikipedia('GPT-4')
+print(data[0].page_content)
