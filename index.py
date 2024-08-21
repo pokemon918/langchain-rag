e98dae422c41a1c75e59ff420e8d1e58916d93b8
Pokemon918 <j27394645@gmail.com>
2024-08-20 17:57:06 -0700

    add: document types

--- index.py
+++ index.py
@@ -3,9 +3,20 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
 
 def load_document(file):
-    from langchain_community.document_loaders import PyPDFLoader
-    print(f'Loading {file}')
-    loader = PyPDFLoader(file)
+    import os
+    name, extension = os.path.splitext(file)
+
+    if extension == '.pdf':
+        from langchain_community.document_loaders import PyPDFLoader
+        print(f'Loading {file}')
+        loader = PyPDFLoader(file)
+    elif extension == '.docx':
+        from langchain_community.document_loaders import Docx2txtLoader
+        print(f'Loading {file}')
+        loader = Docx2txtLoader(file)
+    else:
+        print('Document format is not supported!')
+        return None
     data = loader.load()
     return data
 
@@ -14,4 +25,4 @@ print(data[1].page_content)
 print(data[10].metadata)
 
 print(f'You have {len(data)} pages in your data')
-print(f'There are {len(data[20].page_content)} characters in the page')
+print(f'There are {len(data[20].page_content)} characters in the page') 
